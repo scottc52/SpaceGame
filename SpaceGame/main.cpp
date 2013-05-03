9a4c25edbb98f8eb872eaa70b02a9fb10d6014c4
Austin Griffith Hulse <ahulse@stanford.edu>
2013-05-03 15:35:10 -0700

    Added files to Repo!

---
+++ SpaceGame/main.cpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////
+// INCLUDES
+//////////////////////////////////////////////////////
+
+/* Include windows only for windows users :P
+*/
+#include "windows.h"
+#include <glew.h>
+#include "gl/GL.h"
+#include <glut.h>
+#include <cassert>
+#include <iostream>
+//// OpenMeshIncludes
+#undef min
+#undef max
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
+using namespace std;
+using namespace OpenMesh;
+
+int main(int _argc, char** _argv){
+	cout<<"Hello, world!\n";
+	cin.ignore(1);
+	return 0;
+}
