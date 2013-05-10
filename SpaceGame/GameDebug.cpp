0bcf676ead562f943234d7985e88e9f969f818e6
Austin Griffith Hulse <ahulse@stanford.edu>
2013-05-09 23:48:50 -0700

    FINALLY got GameDebug.cpp included

---
+++ SpaceGame/GameDebug.cpp
@@ -0,0 +1,49 @@
+/*
+ *
+ * Singleton source code for debug
+ *
+ */
+#include "GameDebug.h"
+
+ofstream outFile;
+GameDebugger* GameDebugger::m_pinstance = NULL;
+time_t timer;
+
+GameDebugger* GameDebugger::GetInstance(){
+	//Lock here.
+	if(!m_pinstance){
+		m_pinstance = new GameDebugger;
+	}
+	return m_pinstance;
+}
+
+bool GameDebugger::OpenDebugFile(){
+	//Lock here
+	if(outFile.is_open())return true;
+	outFile.open(GAME_DEBUG_OUTPUT_FILE);
+	return(outFile.is_open());
+}
+
+bool GameDebugger::CloseDebugFile(){
+	//Lock here
+	if(!outFile.is_open())return true;
+	outFile.close();
+	return(!outFile.is_open());
+}
+
+void GameDebugger::WriteDebugMessageToConsole(char* message, int lineNo){
+	//Lock here
+	cout<<"-----------------------------------------------------"<<endl;
+	cout<<message<<" @ line: "<<lineNo<<endl;
+	time(&timer);
+	cout<<"*Found at: "<<ctime(&timer)<<endl;
+}
+
+void GameDebugger::WriteToDebugFile(char* message, int lineNo){
+	if(!outFile.is_open()) return;
+	//Lock here;
+	outFile<<"-----------------------------------------------------"<<endl;
+	outFile<<message<<" @ line: "<<lineNo<<endl;
+	time(&timer);
+	outFile<<"*Found at: "<<ctime(&timer)<<endl;
+}
