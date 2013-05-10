5eba10445caac404eeba8fba3e65247d218e2329
Austin Griffith Hulse <ahulse@stanford.edu>
2013-05-09 20:24:38 -0700

    Added some additional .h files.  Cleaned them up a little.  Also created a header for defining our filesystem locations

---
+++ SpaceGame/GameWorldObject.h
@@ -0,0 +1,11 @@
+/* 
+ *
+ * Basic World Object class.  These objects do not move during the game (Walls, etc.).
+ */
+#ifndef _GAMEWORLDOBJECT_H_
+#define _GAMEWORLDOBJECT_H_
+#include "GameObject.h"
+
+class GameWorldObject : public GameObject{
+};
+#endif
---
+++ SpaceGame/LocationDefines.h
@@ -0,0 +1,7 @@
+//A simple file for defining filesystem location data
+#define GAME_DATA_ROOT_FOLDER ("GameData")
+#define GAME_DATA_MESH_FOLDER ("GameData/Meshes")
+#define GAME_DATA_MATERIALS_FOLDER ("GameData/Materials")
+#define GAME_DATA_TEXTURES_FOLDER ("GameData/Textures")
+#define GAME_DATA_ROOMS_FOLDER ("GameData/Rooms")
+//TO do: Behavior files....
