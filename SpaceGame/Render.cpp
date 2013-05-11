a429970c9963b80c706822332a64476daf654fc0
Scott Chung <scottc52@gmail.com>
2013-05-11 02:10:33 -0700

    Renderer update. Not linked with rest of api yet.

--- SpaceGame/Render.cpp
+++ SpaceGame/Render.cpp
@@ -5,6 +5,29 @@
 */
 #include "Render.h"
 
+//****************************************************
+// classes
+//****************************************************
+class Material{
+public:
+	float Ns;
+	float Tr;
+	float Ka[3];
+	float Kd[3];
+	float Ks[3];
+	float Ke[3];
+};
+Material exampleMaterial(){
+	Material material;
+	material.Ns = 10.0;
+	material.Tr = 0.0;
+	material.Ka[0] = 0.588; material.Ka[1] = 0.588; material.Ka[2] = 0.588;
+	material.Kd[0] = 0.588; material.Kd[1] = 0.588; material.Kd[2] = 0.588;
+	material.Ks[0] = 0.000; material.Ks[1] = 0.000; material.Ks[2] = 0.000; 
+	material.Ke[0] = 0.000; material.Ke[1] = 0.000; material.Ke[2] = 0.000;
+
+	return material;
+}
 
 //****************************************************
 // reshape viewport if the window is resized
@@ -50,10 +73,10 @@ MyMesh squareMesh(){
 
 	vhandle[0] = mesh.add_vertex(MyMesh::Point(-1, -1,  1));
 	vhandle[1] = mesh.add_vertex(MyMesh::Point( 1, -1,  1));
-	vhandle[2] = mesh.add_vertex(MyMesh::Point( 0.5,  0.5,  1));
+	vhandle[2] = mesh.add_vertex(MyMesh::Point( 1,  1,  1));
 	vhandle[3] = mesh.add_vertex(MyMesh::Point(-1,  1,  1));
 	vhandle[4] = mesh.add_vertex(MyMesh::Point(-1, -1, -1));
-	vhandle[5] = mesh.add_vertex(MyMesh::Point( 0.5, -0.5, -1));
+	vhandle[5] = mesh.add_vertex(MyMesh::Point( 1, -1, -1));
 	vhandle[6] = mesh.add_vertex(MyMesh::Point( 1,  1, -1));
 	vhandle[7] = mesh.add_vertex(MyMesh::Point(-1,  1, -1));
 
@@ -66,45 +89,92 @@ MyMesh squareMesh(){
 	face_vhandles.push_back(vhandle[0]);
 	face_vhandles.push_back(vhandle[1]);
 	face_vhandles.push_back(vhandle[2]);
+	//face_vhandles.push_back(vhandle[3]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[0]);
+	//face_vhandles.push_back(vhandle[1]);
+	face_vhandles.push_back(vhandle[2]);
 	face_vhandles.push_back(vhandle[3]);
 	mesh.add_face(face_vhandles);
+
 
 	face_vhandles.clear();
 	face_vhandles.push_back(vhandle[7]);
 	face_vhandles.push_back(vhandle[6]);
 	face_vhandles.push_back(vhandle[5]);
+	//face_vhandles.push_back(vhandle[4]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[7]);
+	//face_vhandles.push_back(vhandle[6]);
+	face_vhandles.push_back(vhandle[5]);
 	face_vhandles.push_back(vhandle[4]);
 	mesh.add_face(face_vhandles);
+
 
 	face_vhandles.clear();
 	face_vhandles.push_back(vhandle[1]);
 	face_vhandles.push_back(vhandle[0]);
 	face_vhandles.push_back(vhandle[4]);
+	//face_vhandles.push_back(vhandle[5]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[1]);
+	//face_vhandles.push_back(vhandle[0]);
+	face_vhandles.push_back(vhandle[4]);
 	face_vhandles.push_back(vhandle[5]);
 	mesh.add_face(face_vhandles);
+
 
 	face_vhandles.clear();
 	face_vhandles.push_back(vhandle[2]);
 	face_vhandles.push_back(vhandle[1]);
 	face_vhandles.push_back(vhandle[5]);
+	//face_vhandles.push_back(vhandle[6]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[2]);
+	//face_vhandles.push_back(vhandle[1]);
+	face_vhandles.push_back(vhandle[5]);
 	face_vhandles.push_back(vhandle[6]);
 	mesh.add_face(face_vhandles);
+
 
 	face_vhandles.clear();
 	face_vhandles.push_back(vhandle[3]);
 	face_vhandles.push_back(vhandle[2]);
 	face_vhandles.push_back(vhandle[6]);
+	//face_vhandles.push_back(vhandle[7]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[3]);
+	//face_vhandles.push_back(vhandle[2]);
+	face_vhandles.push_back(vhandle[6]);
 	face_vhandles.push_back(vhandle[7]);
 	mesh.add_face(face_vhandles);
+
 
 	face_vhandles.clear();
 	face_vhandles.push_back(vhandle[0]);
 	face_vhandles.push_back(vhandle[3]);
 	face_vhandles.push_back(vhandle[7]);
+	//face_vhandles.push_back(vhandle[4]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[0]);
+	//face_vhandles.push_back(vhandle[3]);
+	face_vhandles.push_back(vhandle[7]);
 	face_vhandles.push_back(vhandle[4]);
 	mesh.add_face(face_vhandles);
 
-
+	 mesh.request_vertex_normals();
 	return mesh;
 }
 
@@ -116,7 +186,8 @@ void myDisplay() {
 	int w = glutGet(GLUT_WINDOW_WIDTH);
 	int h = glutGet(GLUT_WINDOW_HEIGHT);
 	float aspect_ratio = (float) w / (float) h;
-	gluPerspective(45.0f, aspect_ratio, 0.01f, 600.0f);
+	float fieldOfView = 45.0f; //TODO
+	gluPerspective(fieldOfView, aspect_ratio, 0.01f, 600.0f);
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Clear to black, fully transparent
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -125,11 +196,10 @@ void myDisplay() {
 
 	glMatrixMode(GL_MODELVIEW);					// indicate we are specifying camera transformations
 	glLoadIdentity();
-	//gluLookAt(5.0f, 0.0f, -zoomOut, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
-	float cameraPos[4] = {0,0,4,1};
-	Vector3f up = Vector3f(0,1,0);
-	Vector3f pan = Vector3f(0,0,0);
-	gluLookAt(cameraPos[0]+pan[0], cameraPos[1]+pan[1], cameraPos[2]+pan[2], pan[0], pan[1], pan[2], up[0], up[1], up[2]);
+	Vector3f cameraPos = Vector3f(0,0,4); //TODO
+	Vector3f up = Vector3f(0,1,0); //TODO
+	Vector3f direction = Vector3f(0,0,-1); //TODO
+	gluLookAt(cameraPos[0], cameraPos[1], cameraPos[2], cameraPos[0]+direction[0], cameraPos[1]+direction[1], cameraPos[2]+direction[2], up[0], up[1], up[2]);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);				// clear the color buffer (sets everything to black)
 
 	//glMatrixMode(GL_MODELVIEW);					// indicate we are specifying camera transformations
@@ -140,59 +210,97 @@ void myDisplay() {
 
 	//enabling lighting/ shading
 	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	GLfloat ambientlight[] = {0.3,0.3,0.3,1.0};
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientlight);
+	unsigned int gl_lights[] = {GL_LIGHT0, GL_LIGHT1, GL_LIGHT2, GL_LIGHT3, GL_LIGHT4,
+		GL_LIGHT5, GL_LIGHT6, GL_LIGHT7};
+	int numLights = 1; //TODO
+	for(int i = 0; i<numLights; i++){
+		//TODO: all these variables must depend on game room lights 
+		glEnable(gl_lights[0]);
+		GLfloat ambientlight[] = {0.3,0.3,0.3,1.0};
+		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientlight);
 
-	//light colors
-	GLfloat ambientcolor[] = {0.2, 0.2, 0.2, 1.0};
-	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientcolor);
-	GLfloat diffusecolor[] = {0.1, 0.5, 0.8, 1.0};
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffusecolor);
-	GLfloat specularcolor[] = {0.8, 0.8, 0.8, 1.0};
-	glLightfv(GL_LIGHT0, GL_SPECULAR, specularcolor);
+		//light colors
+		GLfloat ambientcolor[] = {0.2, 0.2, 0.2, 1.0};
+		glLightfv(gl_lights[0], GL_AMBIENT, ambientcolor);
+		GLfloat diffusecolor[] = {0.8, 0.8, 0.8, 1.0};
+		glLightfv(gl_lights[0], GL_DIFFUSE, diffusecolor);
+		GLfloat specularcolor[] = {0.8, 0.8, 0.8, 1.0};
+		glLightfv(gl_lights[0], GL_SPECULAR, specularcolor);
 
-	GLfloat lightposition[] = {300.0, 300.0, 300.0, 1.0};
-	glLightfv(GL_LIGHT0, GL_POSITION, lightposition);
+		GLfloat lightposition[] = {300.0, 300.0, 300.0, 1.0};
+		if(true){ //positional
+			lightposition[3] = 1.0;
+		}else{//directional
+			lightposition[3] = 0.0;
+		}
+		glLightfv(gl_lights[0], GL_POSITION, lightposition);
 
-	GLfloat lightdirection[] = {-1.0,-1.0,-1.0};
-	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, lightdirection);
-	glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 30.0);
+		if(true){ // should have a setting for this
+			GLfloat lightdirection[] = {-1.0,-1.0,-1.0};
+			glLightfv(gl_lights[0], GL_SPOT_DIRECTION, lightdirection);
+			glLightf(gl_lights[0], GL_SPOT_CUTOFF, 30.0);
+		}
+	}
 
-
-
-
-	glEnable(GL_TEXTURE_2D);
-	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-	//glBindTexture(GL_TEXTURE_2D, textureId);
 
 
 
 
 	//----------------------- code to draw objects --------------------------
-	MyMesh mesh = squareMesh();
-	glBegin(GL_QUADS);
-	glColor3f(.5,.5,.5);
-	for (MyMesh::FaceIter it = mesh.faces_begin(); it != mesh.faces_end(); ++it) {
+	int numObjects = 1;
+	for(int i = 0; i<numObjects;i++){
+		MyMesh mesh = squareMesh();
+		bool useCustomShader = false; //TODO
+		if(useCustomShader){ //check if you should use custom shader
+			GLuint programID = 1; //TODO
+			//glUseProgram(programID); //TODO: this gives errors. Not sure why. Something to do with linking with glew.
+		}else{
+			//glUseProgram(0); //standard openGL shader
+		}
+		
+		//set materials and textures
+		//TODO all material properties should be read in from object
+		Material material = exampleMaterial();
+		GLfloat ambientmat[] = { material.Ka[0], material.Ka[1], material.Ka[2], 1.0-material.Tr };
+		glMaterialfv(GL_FRONT, GL_AMBIENT, ambientmat);
+		GLfloat diffusemat[] = { material.Kd[0], material.Kd[1], material.Kd[2], 1.0-material.Tr };
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, diffusemat);
+		GLfloat specularmat[] = { material.Ks[0], material.Ks[1], material.Ks[2], 1.0-material.Tr };
+		glMaterialfv(GL_FRONT, GL_SPECULAR, specularmat);
+		GLfloat shinimat[] = {material.Ns};
+		glMaterialfv(GL_FRONT, GL_SHININESS, shinimat);
+		GLfloat emmisionmat[] = { material.Ke[0], material.Ke[1], material.Ke[2], 1.0-material.Tr };
+		glMaterialfv(GL_FRONT, GL_EMISSION, emmisionmat);
 
-		MyMesh::HalfedgeHandle it2 = mesh.halfedge_handle(it.handle());
-		Vec3f p = mesh.point(mesh.to_vertex_handle(it2));
-		//shadeByCurvature(mesh.to_vertex_handle(it2));
-		glVertex3f(p[0],p[1],p[2]);
-		it2 = mesh.next_halfedge_handle(it2);
-		p = mesh.point(mesh.to_vertex_handle(it2));
-		//shadeByCurvature(mesh.to_vertex_handle(it2));
-		glVertex3f(p[0],p[1],p[2]);
-		it2 = mesh.next_halfedge_handle(it2);
-		p = mesh.point(mesh.to_vertex_handle(it2));
-		//shadeByCurvature(mesh.to_vertex_handle(it2));
-		glVertex3f(p[0],p[1],p[2]);
-		it2 = mesh.next_halfedge_handle(it2);
-		p = mesh.point(mesh.to_vertex_handle(it2));
-		//shadeByCurvature(mesh.to_vertex_handle(it2));
-		glVertex3f(p[0],p[1],p[2]);
+		bool useTexture = false  && mesh.has_vertex_normals(); //TODO
+		if(useTexture){
+			glEnable(GL_TEXTURE_2D);
+			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+			GLuint textureID = 0; //TODO
+			glBindTexture(GL_TEXTURE_2D, textureID);
+		}else{
+			glDisable(GL_TEXTURE_2D);
+		}
+
+		glBegin(GL_TRIANGLES);
+		for (MyMesh::FaceIter it = mesh.faces_begin(); it != mesh.faces_end(); ++it) {
+			//assuming triangular meshes
+			MyMesh::HalfedgeHandle it2 = mesh.halfedge_handle(it.handle());
+			for(int v = 0; v< 3; v++){
+				MyMesh::VertexHandle v_handle = mesh.to_vertex_handle(it2);
+				if(useTexture){
+					Vec3f avg =mesh.normal(v_handle);
+					glNormal3f(avg[0], avg[1], avg[2]);
+					Vec2f texCoord; //TODO
+					glTexCoord2f(texCoord[0], texCoord[1]);
+				}
+				Vec3f p = mesh.point(v_handle);
+				glVertex3f(p[0],p[1],p[2]);
+				it2 = mesh.next_halfedge_handle(it2);
+			}
+		}
+		glEnd();
 	}
-	glEnd();
 	//-----------------------------------------------------------------------
 
 	glFlush();
