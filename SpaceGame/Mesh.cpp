7e6e17704017e4198866d34df80e8635d2f6908f
Jon Tatum <jdtatum@stanford.edu>
2013-05-27 16:34:52 -0700

    mesh.cpp fix

--- SpaceGame/Mesh.cpp
+++ SpaceGame/Mesh.cpp
@@ -13,9 +13,9 @@ bool MyMeshIO::LoadMesh(MyMesh &m, const string &s, IO::Options opt){
 	if(!p_instance){
 		p_instance = new IO::_OBJReader_();
 	}
-	bool = IO::read_mesh(m, s, opt); 
-	mesh->request_vertex_normals();
-	mesh->request_face_normals();
-	mesh->update_normals();	
-	return 
+	bool ret = IO::read_mesh(m, s, opt); 
+	m.request_vertex_normals();
+	m.request_face_normals();
+	m.update_normals();	
+	return ret;
 }
