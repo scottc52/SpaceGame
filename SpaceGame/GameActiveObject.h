89fecee76529cc3f2452ca343cfce6f9ee168a48
Austin Griffith Hulse <ahulse@stanford.edu>
2013-05-30 16:15:11 -0700

    Overhaul, added skeletal code for GamePlayer.h and GameState,h

--- SpaceGame/GameActiveObject.h
+++ SpaceGame/GameActiveObject.h
@@ -15,14 +15,23 @@
 
 class GameActiveObject : public GameObject{
 private:
-	Vec3f CenterOfMass;
+	Vec3f com;
+	double totalMass;
 	string state;
+	Vec3f velocity;
+	//Rotation information
 public:
 	GameActiveObject::GameActiveObject(){
+		velocity = Vec3f(0,0,0);
 		state = "neutral";
 	}
 
 	void GameActiveObject::SetState(const char* s){state = s;}
 	const char* GameActiveObject::GetState(){return state.c_str();}
+	double GameActiveObject::GetMass(){return totalMass;}
+	void GameActiveObject::GetMass(const double& m){totalMass = m;}
+	Vec3f GameActiveObject::CenterOfMass(){return com;}
+	void GameActiveObject::CalculateCenterOfMass();
+	void GameActiveObject::IncrementVelocity(Vec3f& deltaV){velocity += deltaV;}
 };
 #endif _GAMEACTIVEOBJECT_H_
